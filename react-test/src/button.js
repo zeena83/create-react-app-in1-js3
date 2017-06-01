4edbae0bbaf8b71871763fc719a8f508d9ee6040
zeena83 <zeena_aywaz@yahoo.com>
2017-06-01 10:40:14 +0200

    component-button

---
+++ react-test/src/button.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  constructor(props) {
+      super(props);
+    }
+  render() {
+    return (
+        <div>
+            <button onClick={this.props.buttonClear}
+            type="reset" className="buttonClass" >Clear</button>
+        </div>
+      );
+  }
+}
+
+export default Button;
