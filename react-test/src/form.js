97e5fc96639418c5631dc3965db65eee161362eb
zeena83 <zeena_aywaz@yahoo.com>
2017-06-01 10:34:55 +0200

    component-form

---
+++ react-test/src/form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Form extends Component {
+  render() {
+    return (
+        <div>
+            <input value={this.props.name} className="name" onChange={this.props.inputName} placeholder="name" />
+            <input value={this.props.email} className="email" onChange={this.props.inputEmail} placeholder="email" />
+        </div>
+      );
+  }
+    
+}
+
+export default Form;
