dcc2aea635dc1ba3f51dfe243b3cd35f36a771f9
zeena83 <zeena_aywaz@yahoo.com>
2017-06-01 10:30:18 +0200

    test-form

--- react-test/src/App.test.js
+++ react-test/src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Form from './form';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+test('renders input-name', () => {
+  let wrapper = mount(<Form/>);
+  wrapper.find({className:'name'})       
+  });
+
+test('renders input-email', () => {
+   let wrapper = mount(<Form/>); 
+   wrapper.find({className:'email'})
+  });
+
+
